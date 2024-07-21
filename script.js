7bc323f7de0855a1e2cc518a88c9c3434f1a9f76
thedicrey <dicreyraptor@gmail.com>
2024-07-21 13:43:53 -0500

    Web 1.2

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded",function(){
-    // const imgCapura = document.querySelector(".img-captura")
+    const imgCapura = document.querySelector(".img-captura")
+    const btnExit = document.querySelector(".btn-exit")
 
-    // imgCapura.addEventListener("click",() => {
-    //         imgCapura.classList.toggle("img-transition")
-    // })
+    imgCapura.addEventListener("click",() => {
+        imgCapura.classList.add("img-transition")
+        btnExit.classList.add("exit")
+    })
+    btnExit.addEventListener("click",() => {
+        imgCapura.classList.remove("img-transition")
+        btnExit.classList.remove("exit")
+    })
 })
