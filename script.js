c773e59627334d7eedcaad928749d6ce5401ed87
thedicrey <dicreyraptor@gmail.com>
2024-07-21 12:42:51 -0500

    Web 1.1

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded",function(){
+    // const imgCapura = document.querySelector(".img-captura")
+
+    // imgCapura.addEventListener("click",() => {
+    //         imgCapura.classList.toggle("img-transition")
+    // })
+})
